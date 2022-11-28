64019889f16abac67e8c1fe2ebd2376034094bd3
Adam K Dean <adamkdean@googlemail.com>
2022-11-28 00:43:20 +0000

    fix import path issue

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@
 // license that can be found in the LICENSE file.
 
 import Express from 'express'
-import Package from '../package.json'
+import Package from '../../package.json'
 import http from 'http'
 
 export class API {
